c3a68774c8f3a54178f15af844c88cda4f12e180
tuankietp313-wq <tuankietp313@gmail.com>
2026-04-15 02:52:13 +0700

    first commit

--- Ordering-Application/Product.h
+++ Ordering-Application/Product.h
@@ -10,13 +10,22 @@ private:
     int stock;
 public:
     // VÙNG CODE CỦA TV1: Constructor, Getters, Setters (Encapsulation)
+  // Constructor
     Product(std::string i = "", std::string n = "", double p = 0, int s = 0)
         : id(i), name(n), price(p), stock(s) {
     }
 
+    // ===== GETTER =====
     std::string getId() const { return id; }
+    std::string getName() const { return name; }
     double getPrice() const { return price; }
     int getStock() const { return stock; }
+
+    // ===== SETTER =====
+    void setId(const std::string& i) { id = i; }
+    void setName(const std::string& n) { name = n; }
+    void setPrice(double p) { price = p; }
+    void setStock(int s) { stock = s; }
 
     // VÙNG CODE CỦA TV4: Nạp chồng toán tử <<, >> và ==
     friend std::ostream& operator<<(std::ostream& out, const Product& p) {
--- Ordering-Application/ShoppingCart.h
+++ Ordering-Application/ShoppingCart.h
@@ -12,11 +12,37 @@ private:
     std::vector<CartItem> items;
 public:
     // TV1: Viết logic thêm vào giỏ, tính tổng tiền (Subtotal)
-    void addItem(Product p, int q) { items.push_back({ p, q }); }
+    // ===== THÊM SẢN PHẨM =====
+    void addItem(Product p, int q) {
+        // Kiểm tra nếu sản phẩm đã tồn tại thì cộng số lượng
+        for (auto& item : items) {
+            if (item.product == p) {
+                item.quantity += q;
+                return;
+            }
+        }
+        // Nếu chưa có thì thêm mới
+        items.push_back({ p, q });
+    }
+
+    // ===== TÍNH TỔNG TIỀN =====
     double calculateTotal() {
         double total = 0;
-        for (auto& i : items) total += i.product.getPrice() * i.quantity;
+        for (const auto& i : items) {
+            total += i.product.getPrice() * i.quantity;
+        }
         return total;
     }
-    const std::vector<CartItem>& getItems() const { return items; }
+
+    // ===== LẤY DANH SÁCH =====
+    const std::vector<CartItem>& getItems() const {
+        return items;
+    }
+    //======HIỂN THỊ GIỎ HÀNG====
+    void displayCart() {
+    for (const auto& i : items) {
+        std::cout << i.product << " | SL: " << i.quantity << std::endl;
+    }
+    std::cout << "Total: " << calculateTotal() << std::endl;
+}
 };

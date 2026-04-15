b6f6827fc64d6c878736d09ffc8ca2233b10385f
tuankietp313-wq <tuankietp313@gmail.com>
2026-04-15 12:56:38 +0700

    update encapsulation

--- Ordering-Application/Product.h
+++ Ordering-Application/Product.h
@@ -13,11 +13,41 @@ public:
     Product(std::string i = "", std::string n = "", double p = 0, int s = 0)
         : id(i), name(n), price(p), stock(s) {
     }
-
     std::string getId() const { return id; }
+    std::string getName() const { return name; }
     double getPrice() const { return price; }
     int getStock() const { return stock; }
-
+    void setId(std::string i) {
+        if (!i.empty())
+            id = i;
+        else
+            std::cout << "ID khong hop le!";
+    }
+    void setName(std::string n) {
+        if (!n.empty())
+            name = n;
+        else
+            std::cout << "Ten khong hop le!";
+    }
+    void setPrice(double p) {
+        if (p >= 0 && p <= 1e9)
+            price = p;
+        else
+            std::cout << "Gia khong hop le!";
+    }
+  void setStock(int s) {
+        if (s >= 0 && s <= 10000)
+            stock = s;
+        else
+            std::cout << "So luong khong hop le!";
+    }
+    void decreaseStock(int quantity) {
+    if (quantity > 0 && quantity <= stock) {
+        stock -= quantity;
+    } else {
+        std::cout << "So luong khong hop le hoac khong du hang!\n";
+    }
+}
     // VÙNG CODE CỦA TV4: Nạp chồng toán tử <<, >> và ==
     friend std::ostream& operator<<(std::ostream& out, const Product& p) {
         out << std::left << std::setw(10) << p.id << std::setw(20) << p.name << p.price;

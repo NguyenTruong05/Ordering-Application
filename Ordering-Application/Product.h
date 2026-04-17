7f6576777711801ca0d775e59e14e1c00bcbd685
NguyenTruong05 <truongdinhnguyen2005@gmail.com>
2026-04-18 01:32:43 +0700

    fix conflix

--- Ordering-Application/Product.h
+++ Ordering-Application/Product.h
@@ -2,60 +2,57 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <limits>
 
 class Product {
 private:
     std::string id, name;
     double price;
     int stock;
+
 public:
-    // VÙNG CODE CỦA TV1: Constructor, Getters, Setters (Encapsulation)
+    // TV1 (KIỆT): Constructor & Đóng gói
     Product(std::string i = "", std::string n = "", double p = 0, int s = 0)
         : id(i), name(n), price(p), stock(s) {
     }
+
     std::string getId() const { return id; }
     std::string getName() const { return name; }
     double getPrice() const { return price; }
     int getStock() const { return stock; }
-    void setId(std::string i) {
-        if (!i.empty())
-            id = i;
-        else
-            std::cout << "ID khong hop le!";
-    }
-    void setName(std::string n) {
-        if (!n.empty())
-            name = n;
-        else
-            std::cout << "Ten khong hop le!";
-    }
-    void setPrice(double p) {
-        if (p >= 0 && p <= 1e9)
-            price = p;
-        else
-            std::cout << "Gia khong hop le!";
-    }
-  void setStock(int s) {
-        if (s >= 0 && s <= 10000)
-            stock = s;
-        else
-            std::cout << "So luong khong hop le!";
-    }
+
+    void setId(const std::string& i) { id = i; }
+    void setName(const std::string& n) { name = n; }
+    void setPrice(double p) { price = p; }
+    void setStock(int s) { stock = s; }
+
     void decreaseStock(int quantity) {
-    if (quantity > 0 && quantity <= stock) {
-        stock -= quantity;
-    } else {
-        std::cout << "So luong khong hop le hoac khong du hang!\n";
+        if (quantity > 0 && quantity <= stock) {
+            stock -= quantity;
+        }
+        else {
+            std::cout << "So luong khong hop le hoac khong du hang!\n";
+        }
     }
-}
-    // VÙNG CODE CỦA TV4: Nạp chồng toán tử <<, >> và ==
+
+    //TV4 (PHI): Nạp chồng toán tử
     friend std::ostream& operator<<(std::ostream& out, const Product& p) {
         out << std::left
             << std::setw(10) << p.id
             << std::setw(25) << p.name
-            << std::setw(12) << p.price
-            << std::setw(8) << p.stock;
+            << std::setw(10) << p.price
+            << std::setw(10) << p.stock;
         return out;
+    }
+
+    friend std::istream& operator>>(std::istream& in, Product& p) {
+        std::getline(in, p.id, ',');
+        std::getline(in, p.name, ',');
+        in >> p.price;
+        in.ignore(1); // bỏ dấu ,
+        in >> p.stock;
+        in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return in;
     }
 
     bool operator==(const Product& other) const {
